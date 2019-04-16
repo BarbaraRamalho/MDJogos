e3c4ed0f9cc343cf9351fc5bea89a2c6392af78f
Henrique Fonseca <henrique.c.fonseca@tecnico.ulisboa.pt>
2019-04-16 01:16:27 +0100

    minor update

---
+++ New Test/Assets/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour
+{
+    public LayerMask movementMask;
+    public Camera cam;
+    PlayerMotor motor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main;
+        motor = GetComponent<PlayerMotor>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if(Physics.Raycast(ray, out hit, 100, movementMask))
+            {
+                Debug.Log("hit " + hit.collider.name + " " + hit.point);
+                motor.MoveToPoint(hit.point);
+                // Move to place
+
+
+
+                // stop focusing object
+            }
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, 100, movementMask))
+            {
+                // Check if hit interactable and set as focus
+            }
+        }
+    }
+}
