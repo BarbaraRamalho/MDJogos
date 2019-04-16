8532ce6801f7b8337e61e1f07c06429d63501441
Henrique Fonseca <henrique.c.fonseca@tecnico.ulisboa.pt>
2019-04-16 11:00:09 +0100

    bars

--- New Test/Assets/3dassests/Stylized Astronaut/Character/Player.cs
+++ New Test/Assets/3dassests/Stylized Astronaut/Character/Player.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour {
 
@@ -10,6 +11,18 @@ public class Player : MonoBehaviour {
 		public float turnSpeed = 400.0f;
 		private Vector3 moveDirection = Vector3.zero;
 		public float gravity = 20.0f;
+
+        public float health = 100;
+        public float energy = 100;
+        public float motivation = 100;
+
+        public float deathRate;
+        public float energyRate;
+        public float motivationRate;
+
+        public Slider HBar;
+        public Slider Ebar;
+        public Slider MBar;
 
 		void Start () {
 			controller = GetComponent <CharacterController>();
@@ -31,5 +44,30 @@ public class Player : MonoBehaviour {
 			transform.Rotate(0, turn * turnSpeed * Time.deltaTime, 0);
 			controller.Move(moveDirection * Time.deltaTime);
 			moveDirection.y -= gravity * Time.deltaTime;
+
+        HBar.value = this.health;
+        Ebar.value = this.energy;
+        MBar.value = this.motivation;
+
+        energy = energy - (energy * energyRate);
+        motivation = motivation - (motivation * motivationRate);
+
+        if(health<=0)
+        {
+            health = 0;
+            anim.SetTrigger("dead");
+            gameObject.GetComponent<Player>().enabled = false;
+        }
+        if (energy <= 0 || motivation <= 0) {
+            health = health - (deathRate * Time.deltaTime);
+        }
+        if (energy <= 0) energy = 0;
+        if (motivation <= 0) motivation = 0;
+
+
+
+
+
+
 		}
 }
