693d6ba4569d2fbc1a04cc47f8338f1e2e2a529d
Henrique Fonseca <henrique.c.fonseca@tecnico.ulisboa.pt>
2019-04-24 15:29:39 +0100

    depois do lab

---
+++ New Test/Assets/UI elements/LAYOUT_PNGS_SEPARADO/inventory/InventoryScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventoryScript : MonoBehaviour
+{
+    public bool isClosed = true;
+    public Image inventoryBG;
+    Image[] invSlots;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        invSlots = inventoryBG.GetComponentsInChildren<Image>();
+
+        inventoryBG.GetComponent<Image>().enabled = !isClosed;
+        foreach (Image i in invSlots) i.enabled = !isClosed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ActivateInventory();
+    }
+
+    void ActivateInventory()
+    {
+        if(Input.GetKeyDown(KeyCode.I))
+        {
+            if(isClosed)
+            {
+                inventoryBG.GetComponent<Image>().enabled = true;
+                foreach (Image child in invSlots) child.enabled = true;
+
+                isClosed = false;
+            }
+            else {
+                inventoryBG.GetComponent<Image>().enabled = false;
+                foreach (Image child in invSlots) child.enabled = false;
+                isClosed = true;
+            }
+        }
+    }
+
+}
